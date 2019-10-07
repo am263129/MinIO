fb36c2c0ec79f4054dbec07c74b95eeca7bc1ca8
Rolland-K <fashioncstar2019@outlook.com>
2019-10-07 17:03:22 +0700

    change to asw again
    can't fix dependance error

--- minio/app/src/main/java/social/media/minio/MainActivity.java
+++ minio/app/src/main/java/social/media/minio/MainActivity.java
@@ -9,20 +9,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.ClientConfiguration;
-import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
@@ -31,181 +39,76 @@ import java.nio.file.StandardCopyOption;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
-import io.minio.MinioClient;
-import io.minio.errors.ErrorResponseException;
-import io.minio.errors.InsufficientDataException;
-import io.minio.errors.InternalException;
-import io.minio.errors.InvalidArgumentException;
-import io.minio.errors.InvalidBucketNameException;
-import io.minio.errors.InvalidEndpointException;
-import io.minio.errors.InvalidPortException;
-import io.minio.errors.NoResponseException;
-import io.minio.errors.RegionConflictException;
-
 public class MainActivity extends AppCompatActivity {
     public String accessKey = "JqqcQzLPx9A8M6eL";
     public String secretKey = "S77TZka2CDpLWss3";
     public String targetIP = "10.3.1.124:9000";
+    private static String bucketName = "testbucket";
+    private static String keyName = "hosts";
+    private static String uploadFileName = "/etc/hosts";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText targetip = (EditText)findViewById(R.id.taraget_ip);
-        Button ok = (Button)findViewById(R.id.btn_ok);
-        ok.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onClick(View view) {
-//                targetIP = targetip.getText().toString();
-                /*
-                AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        final EditText targetip = (EditText) findViewById(R.id.taraget_ip);
+        Button ok = (Button) findViewById(R.id.btn_ok);
+        AWSCredentials credentials = new BasicAWSCredentials("JqqcQzLPx9A8M6eL", "S77TZka2CDpLWss3");
+        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        clientConfiguration.setSignerOverride("AWSS3V4SignerType");
 
-                ClientConfiguration clientConfig = new ClientConfiguration();
-                clientConfig.setProtocol(Protocol.HTTPS);
+        AmazonS3 s3Client = AmazonS3ClientBuilder
+                .standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:9000", Regions.US_EAST_1.name()))
+                .withPathStyleAccessEnabled(true)
+                .withClientConfiguration(clientConfiguration)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
 
-                AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
-                        "https://10.3.1.124:9000", "us-west-1");
+        try {
+            System.out.println("Uploading a new object to S3 from a file\n");
+            File file = new File("asdasd");
+            // Upload file
+            s3Client.putObject(new PutObjectRequest(bucketName, keyName, file));
 
-                AmazonS3 client = AmazonS3ClientBuilder.standard()
-                        .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                        .withClientConfiguration(clientConfig)
-                        .withEndpointConfiguration(endpointConfiguration).build();
-
-                boolean isExist = client.doesBucketExistV2("upload_csv_minio");
-                if (isExist) {
-                    System.out.println("Bucket already exists.");
-                }
-                else {
-                    client.createBucket("upload_csv_minio");
-                }
-                /*
-                List<Bucket> buckets = client.listBuckets();
-                buckets.forEach(b -> System.out.println(b.getName()));
-                */
-                /*
-                URL url = null;
-                try {
-                    url = new URL(
-                            "http://www.cutestpaw.com/wp-content/uploads/2015/11/My-Cute-Baby-Cat.jpg");
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-                Path tempFile = null;
-                try {
-                    tempFile = Files.createTempFile("cat", ".jpg");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try (InputStream in = url.openStream()) {
-                    Files.copy(in, tempFile, StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                client.putObject("upload_csv_minio", "cat.jpg", tempFile.toFile());
-                */
-                MinioClient minioClient = null;
-                try {
-                    minioClient = new MinioClient("http://10.3.1.124:9000", accessKey,
-                            secretKey);
-                } catch (InvalidEndpointException e) {
-                    e.printStackTrace();
-                } catch (InvalidPortException e) {
-                    e.printStackTrace();
-                }
-                boolean isExist = false;
-                try {
-                    isExist = minioClient.bucketExists("upload_csv_minio");
-                } catch (InvalidBucketNameException e) {
-                    e.printStackTrace();
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                } catch (InsufficientDataException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InvalidKeyException e) {
-                    e.printStackTrace();
-                } catch (NoResponseException e) {
-                    e.printStackTrace();
-                } catch (XmlPullParserException e) {
-                    e.printStackTrace();
-                } catch (ErrorResponseException e) {
-                    e.printStackTrace();
-                } catch (InternalException e) {
-                    e.printStackTrace();
-                }
-                if (isExist) {
-                    System.out.println("Bucket already exists.");
-                }
-                else {
-                    try {
-                        minioClient.makeBucket("upload_csv_minio");
-                    } catch (InvalidBucketNameException e) {
-                        e.printStackTrace();
-                    } catch (RegionConflictException e) {
-                        e.printStackTrace();
-                    } catch (NoSuchAlgorithmException e) {
-                        e.printStackTrace();
-                    } catch (InsufficientDataException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (InvalidKeyException e) {
-                        e.printStackTrace();
-                    } catch (NoResponseException e) {
-                        e.printStackTrace();
-                    } catch (XmlPullParserException e) {
-                        e.printStackTrace();
-                    } catch (ErrorResponseException e) {
-                        e.printStackTrace();
-                    } catch (InternalException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                URL url = null;
-                try {
-                    url = new URL(
-                            "http://www.cutestpaw.com/wp-content/uploads/2015/11/My-Cute-Baby-Cat.jpg");
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-                Path tempFile = null;
-                try {
-                    tempFile = Files.createTempFile("cat", ".jpg");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                try (InputStream in = url.openStream()) {
-                    Files.copy(in, tempFile, StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    minioClient.putObject("upload_csv_minio", "cat.jpg", tempFile.toString());
-                } catch (InvalidBucketNameException e) {
-                    e.printStackTrace();
-                } catch (NoSuchAlgorithmException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InvalidKeyException e) {
-                    e.printStackTrace();
-                } catch (NoResponseException e) {
-                    e.printStackTrace();
-                } catch (XmlPullParserException e) {
-                    e.printStackTrace();
-                } catch (ErrorResponseException e) {
-                    e.printStackTrace();
-                } catch (InternalException e) {
-                    e.printStackTrace();
-                } catch (InvalidArgumentException e) {
-                    e.printStackTrace();
-                } catch (InsufficientDataException e) {
-                    e.printStackTrace();
-                }
+            // Download file
+            GetObjectRequest rangeObjectRequest = new GetObjectRequest(bucketName, keyName);
+            S3Object objectPortion = s3Client.getObject(rangeObjectRequest);
+            System.out.println("Printing bytes retrieved:");
+            try {
+                displayTextInputStream(objectPortion.getObjectContent());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        });
+        } catch (AmazonServiceException ase) {
+            System.out.println("Caught an AmazonServiceException, which " + "means your request made it "
+                    + "to Amazon S3, but was rejected with an error response" + " for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+
+        } catch (AmazonClientException ace) {
+            System.out.println("Caught an AmazonClientException, which " + "means the client encountered " + "an internal error while trying to "
+                    + "communicate with S3, " + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+
+        }
+
+    }
+
+    private void displayTextInputStream(InputStream input) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        while (true) {
+            String line = reader.readLine();
+            if (line == null)
+                break;
+
+            System.out.println("    " + line);
+        }
+        System.out.println();
+
     }
 }
